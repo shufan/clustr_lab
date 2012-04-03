5554062ce81697020f40ff3d4330aa1b5dec7d0e
Tony Zhang <shufan.tsz@gmail.com>
2012-04-03 04:42:19 -0400

    erasing last commit, implemented calculateCentroid, update, and getDistance for dnastrands

--- seqdnastrands.py
+++ seqdnastrands.py
@@ -3,9 +3,6 @@ import math
 import numpy
 import random
 import getopt
-import csv
-
-bases = ['A', 'C', 'G', 'T']
 
 def usage():
     print '$> python seqdatapoints.py <required args> [optional args]\n' + \
@@ -56,63 +53,52 @@ class Cluster:
         self.centroid = self.calculateCentroid()
         return getDistance(old_centroid, self.centroid)
     def calculateCentroid(self):
-        # TONY: write this
-        reduce_coord = lambda i:reduce(lambda x,p : x + p[i],self.points,0.0)
-        centroid_coords = [reduce_coord(i)/len(self.points) for i in range(self.dimension)]
-        return tuple(centroid_coords)
-        # TONY: end
+        centroid_strand = []
+        for i in len(dnastrands[0]):
+            aCount, tCount, cCount, gCount = 0, 0, 0, 0
+            # gather counts
+            for d in dnastrands:
+                if d[i] == 'a':
+                    aCount += 1
+                elif d[i] == 't':
+                    tCount += 1
+                elif d[i] == 'c':
+                    cCount += 1
+                elif d[i] == 'g':
+                    gCount += 1
+            maxCount = max([aCount, tCount, cCount, gCount])
+            # find which had max count
+            mostCommon = []
+            if aCount == maxCount:
+                mostCommon.append('a')
+            elif tCount == maxCount:
+                mostCommon.append('t')
+            elif cCount == maxCount:
+                mostCommon.append('c')
+            elif gCount == maxCount:
+                mostCommon.append('g')
+            if len(mostCommon) > 1:
+                # tie exists, so randomly choose one
+                index = random.randint(0,len(mostCommon)-1)
+                centroid_strand.append(mostCommon[index])
+            else:
+                centroid_strand.append(mostCommon[0])
+        return tuple(centroid_strand)
 
-#creates a centroid with a given length, by randomly selecting bases
-def createCentroid(dnaLength):
-   dna = []
-   for j in range(dnaLength):
-       dna.append(random.choice(bases))
-   return dna
-
-#returns the similarity between two given centroids: 'centroid' & 'pair'
-def similarity(centroid, pair, dnaLength):
-   sameCount = 0
-   for i in range(dnaLength):
-      if centroid[i] == pair[i]:
-         sameCount += 1
-   return sameCount
-
-#returns the most similarities between a given centroid and
-#all other existing centroids
-def maxSimilarity(centroid, initial, dnaLength):
-   maxSim = 0
-   for pair in initial:
-      sim = similarity(centroid, pair, dnaLength)
-      if sim > maxSim:
-         maxSim = sim
-   return maxSim
-
-def kmeans(dnastrands, k, var_cutoff, dnaLength):
+def kmeans(dnastrands, k, var_cutoff):
     """choose k initial centroids randomly, but check
     similarity and regenerate if too similar"""
     # DIXIE: write this
     initial = []
-    maxAllowedSim = int(dnaLength * .75)
-    for i in range(k):
-        centroid = createCentroid(dnaLength)
-        tryCount = 0
-        #is it different enough from other centroids in initial?
-        similarity = maxSimilarity(centroid, initial, dnaLength)
-        minSimSoFar = similarity
-        bestCentroid = centroid
-        while (similarity > maxAllowedSim):
-            #if we've tried too many times to satisfy maxAllowedSim,
-            #just take the most different centroid we've generated
-            if (tryCount == 20):
-                break
-            if (similarity < minSimSoFar):
-                minSimSoFar = similarity
-                bestCentroid = centroid
-            centroid = createCentroid(dnaLength)
-            similarity = maxSimilarity(centroid, initial, dnaLength)
-            tryCount += 1
-        initial.append(bestCentroid)
-
+    min_coord = map(min,zip(*points))
+    max_coord = map(max,zip(*points))
+    dim = len(max_coord)
+    for n in range(1,k+1):
+        c = []
+        for d in range(0,dim):
+            p = (max_coord[d]-min_coord[d])/k*n
+            c.append(p)
+        initial.append(tuple(c))
     # DIXIE: end
     centroids = [Cluster([d]) for d in initial]
     while True:
@@ -134,12 +120,14 @@ def kmeans(dnastrands, k, var_cutoff, dnaLength):
             break
     return centroids
 
-def getDistance(x, y):
-    # TONY: edit this, make it hamming distance
-    if len(x) != len(y): raise Exception("coordinates not in same dimension")
-    ret = reduce(lambda a,b: a + pow((x[b]-y[b]), 2), range(len(x)), 0.0)
-    return math.sqrt(ret)
-    # TONY: end
+def getDistance(s1, s2):
+    # find hamming distance between s1 and s2
+    if len(s1) != len(s2): raise Exception("dnastrands are not the same length")
+    diff = 0
+    for i in len(s1):
+        if s1[i] != s2[i]:
+            diff += 1
+    return diff
 
 def main():
     # TODO: rewrite this to read dnastrand input
@@ -147,13 +135,21 @@ def main():
     # read points from input file
     inputfile = open(inputname, "rb")
     inputstream = csv.reader(inputfile)
-    strands = []
-    for strand in inputstream:
-        strands.append(strand[0])
+    rownum = 0
+    points = []
+    for row in inputstream:
+        colnum = 0
+        for col in row:
+            if colnum == 0:
+                x = float(col)
+            if colnum == 1:
+                y = float(col)
+            colnum += 1
+        point = tuple([x,y])
+        points.append(point)
     inputfile.close()
-    dnaLength = len(strands[0])
     # end
-    clusters = kmeans(dnastrands, k, var_cutoff, dnaLength)
+    clusters = kmeans(dnastrands, k, var_cutoff)
 
     for i,c in enumerate(clusters):
         for d in c.dnastrands:
